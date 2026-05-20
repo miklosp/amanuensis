a3802707732cfbaaf114229cc5cb4e20b80aec83
Miklos <miklos.petravich@gmail.com>
2026-05-20 11:54:21 +0200

    Initial Commit

---
+++ audio-pipeline/audio_pipelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  audio_pipelineApp.swift
+//  audio-pipeline
+//
+//  Created by Miklos Petravich on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct audio_pipelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
